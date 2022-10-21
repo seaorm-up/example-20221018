e0b4e48382fcfee457b76eca71c14e966b9dfc76
Your Name <you@example.com>
2022-10-21 16:31:20 +0800

    0.0.7

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+//! # History
+//! ```
+//! cargo new --lib sdk1018
+//! # code sdk1018
+//! ```
+//!
+//! * git submodule
+//! ```
+//! git submodule add https://github.com/seaorm-up/entities
+//! git submodule add https://github.com/seaorm-up/data-dict
+//! git submodule add https://github.com/seaorm-up/common
+//!
+//! git rm --cached -r -f -- entities
+//! git rm --cached -r -f -- common
+//! git rm --cached -r -f -- data-dict
+//! #.gitignore
+//! ```
